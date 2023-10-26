892c1518c9edc633a8e3c2fa765455d2fc7e8793
Bowen Jiang <jiangbowen@cmu.edu>
2023-10-26 10:37:20 -0400

    Added loan payment and verification

--- contracts/Interest.sol
+++ contracts/Interest.sol
@@ -1,3 +1,4 @@
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 contract Loan {
@@ -19,6 +20,19 @@ contract Loan {
         interest_rate = _interest_rate;
         loan_duration = _loan_duration;
         paid = false;
-        balance = _loaned_amount;
+        balance = 0;
+    }
+
+    function verifyLoan() public view returns (bool) {
+        uint256 applicantBalance = borrower.balance;
+        return applicantBalance >= loaned_amount * (interest_rate ** loan_duration);
+    }
+
+    function initiateLoan() public payable {
+        require(msg.sender == borrower, "Only the borrower can fund the loan");
+        require(msg.value == loaned_amount, "Amount sent must be equal to the loaned amount");
+        paid = true;
+        balance = loaned_amount;
+        // Mint bEth to borrower
     }
 }
