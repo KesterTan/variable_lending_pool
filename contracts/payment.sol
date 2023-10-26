98697481c48946c518ecda5fcbd9201c2c6cd655
pireylow <pirey.low@gmail.com>
2023-10-26 01:11:00 -0400

    smart contract for variable lending rates

--- contracts/payment.sol
+++ contracts/payment.sol
@@ -1,42 +1,49 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract LendingPlatform {
+contract LendingPool {
     address public owner;
+    uint256 public interestRate; // Annual interest rate in percentage (e.g., 5%)
+    uint256 public totalDeposits;
 
-    struct User {
-        uint256 collateralAmount;
+    struct Borrower {
         uint256 borrowedAmount;
-        uint256 creditScore;
-        bool exists;
+        uint256 repaymentAmount;
+        uint256 monthlyRepayment;
     }
 
-    mapping(address => User) public users;
+    mapping(address => uint256) public deposits;
+    mapping(address => Borrower) public borrowers;
 
-    constructor() {
+    constructor(uint256 _interestRate) {
         owner = msg.sender;
+        interestRate = _interestRate;
     }
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Only the owner can call this function");
-        _;
+    function deposit() public payable {
+        deposits[msg.sender] += msg.value;
+        totalDeposits += msg.value;
     }
 
-    function isBorrower(address userAddress) public view returns (bool) {
-        return users[userAddress].exists;
+    function borrow(uint256 amount) public {
+        require(deposits[msg.sender] >= amount, "Insufficient balance");
+        require(borrowers[msg.sender].borrowedAmount == 0, "Already a borrower");
+        
+        uint256 annualInterest = (amount * interestRate) / 100;
+        uint256 monthlyInterest = annualInterest / 12;
+        uint256 monthlyRepayment = (amount + monthlyInterest) / 12;
+
+        borrowers[msg.sender] = Borrower({
+            borrowedAmount: amount,
+            repaymentAmount: amount + annualInterest,
+            monthlyRepayment: monthlyRepayment
+        });
+
+        deposits[msg.sender] -= amount;
     }
 
-    function checkPaymentAbility(address borrowerAddress, uint256 interestRate) public view returns (bool, uint256) {
-        User storage borrower = users[borrowerAddress];
-        require(borrower.exists, "User does not exist");
-
-        // Calculate the monthly payment based on the interest rate
-        uint256 borrowedAmount = borrower.borrowedAmount;
-        uint256 monthlyPayment = (borrowedAmount * interestRate) / 12; // Assuming annual interest rate
-
-        // Check if the user has enough collateral to make monthly payments
-        bool canAfford = borrower.collateralAmount >= monthlyPayment;
-
-        return (canAfford, monthlyPayment);
+    function canMakePayment() public view returns (bool) {
+        Borrower storage borrower = borrowers[msg.sender];
+        return borrower.repaymentAmount > 0 && deposits[msg.sender] >= borrower.monthlyRepayment;
     }
 }
