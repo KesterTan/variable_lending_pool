6f11e8b7f4dc6ef1fbdabdf80e4c5f09755c3b1f
yl10016 <yanlin10016@gmail.com>
2023-10-27 17:48:20 -0400

    Interest rate function

--- contracts/Interest.sol
+++ contracts/Interest.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT 
 pragma solidity ^0.8.0;
 
 contract Loan {
@@ -20,5 +21,34 @@ contract Loan {
         loan_duration = _loan_duration;
         paid = false;
         balance = _loaned_amount;
+    }
+}
+
+contract LoanInterestCalculator {
+    uint256 public riskPremium = 110; // Represents a 10% premium (will get divided by 100 later)
+
+    function calculateInterest(uint256 amount, uint256 exchangeRate, uint256 creditScore) public view returns (uint256) {
+        // Calculate interest based on the risk premium and credit score
+        uint256 baseInterest = (amount * riskPremium) / (100 * exchangeRate);
+        uint256 creditScoreFactor;
+
+        // Adjust the credit score factor based on credit score
+        if (creditScore >= 800) {
+            creditScoreFactor = 1; // Excellent credit
+        } else if (creditScore >= 700) {
+            creditScoreFactor = 2; // Good credit
+        } else if (creditScore >= 600) {
+            creditScoreFactor = 3; // Fair credit
+        } else {
+            creditScoreFactor = 4; // Poor credit
+        }
+
+        return baseInterest * creditScoreFactor;
+    }
+
+    function setRiskPremium(uint256 newRiskPremium) external {
+        // Only the owner or an authorized party can set the risk premium
+        // Add access control as needed
+        riskPremium = newRiskPremium;
     }
 }
